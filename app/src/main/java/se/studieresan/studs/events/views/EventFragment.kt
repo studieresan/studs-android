92c240a55b2608cc842445dc7406a3a5d07975bd
Sven Anderzén <svenanderzen@users.noreply.github.com>
2018-12-28 22:25:09 +0100

    Merge upcoming and past events tabs into one event tab

--- app/src/main/java/se/studieresan/studs/events/views/EventFragment.kt
+++ app/src/main/java/se/studieresan/studs/events/views/EventFragment.kt
@@ -15,32 +15,4 @@ class EventFragment : Fragment() {
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
     return inflater.inflate(R.layout.fragment_event, container, false)
   }
-
-  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    super.onViewCreated(view, savedInstanceState)
-
-    event_tabs.setupWithViewPager(event_pager)
-    event_pager.adapter = EventPagerAdapter(childFragmentManager)
-  }
-
-  private inner class EventPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
-    override fun getItem(position: Int): Fragment {
-      when (position) {
-        0 -> return UpcomingEventsFragment()
-        1 -> return PastEventsFragment()
-      }
-      throw IndexOutOfBoundsException("Unknown tab: $position")
-    }
-
-    override fun getCount(): Int = 2
-
-    override fun getPageTitle(position: Int): CharSequence? {
-      when (position) {
-        0 -> return resources.getString(R.string.upcoming)
-        1 -> return resources.getString(R.string.past)
-      }
-      throw IndexOutOfBoundsException("Unknown tab: $position")
-    }
-  }
-
 }
