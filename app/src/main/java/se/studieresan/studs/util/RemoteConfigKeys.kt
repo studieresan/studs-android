ec914d801c91ebe78178c2ec4e8c9b4759523df9
Fanny Curtsson <curtsson@kth.se>
2020-12-15 01:12:54 +0100

    removes unneccessary utils + adds dark mode handling for status bar

--- app/src/main/java/se/studieresan/studs/util/RemoteConfigKeys.kt
+++
@@ -1,6 +0,0 @@
-package se.studieresan.studs.util
-
-object RemoteConfigKeys {
-    const val SEND_NOTIFICATIONS_ENABLED = "send_notifications_enabled"
-    const val IN_APP_FORMS_ENABLED = "in_app_forms_enabled"
-}
