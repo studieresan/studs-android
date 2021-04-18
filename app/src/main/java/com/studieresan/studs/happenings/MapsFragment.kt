35010bafe2eeaca007bdb52a2a8842265103fed5
fcurtsson <fanny@curtsson.com>
2021-04-19 00:48:59 +0200

    Adds markers for happenings on map

--- app/src/main/java/com/studieresan/studs/happenings/MapsFragment.kt
+++ app/src/main/java/com/studieresan/studs/happenings/MapsFragment.kt
@@ -1,24 +1,24 @@
 package com.studieresan.studs.happenings
 
-import androidx.fragment.app.Fragment
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.studieresan.studs.R
+import com.studieresan.studs.data.models.Happenings
 import com.studieresan.studs.happenings.viewmodels.HappeningsViewModel
 
-class MapsFragment : Fragment() {
 
-    //private var viewModel: HappeningsViewModel?=null
+class MapsFragment : Fragment() {
 
     private val callback = OnMapReadyCallback { googleMap ->
         /**
@@ -30,24 +30,44 @@ class MapsFragment : Fragment() {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        // change this to maybe first happening?
+        val stockholm = LatLng(59.3, 18.0)
+        googleMap.addMarker(MarkerOptions().position(stockholm).title("sthlm"))
+
+        val viewModel= ViewModelProviders.of(requireActivity()).get(HappeningsViewModel::class.java)
+
+        viewModel.happenings.observe(viewLifecycleOwner, Observer<Happenings> { t ->
+            val happenings = t.data.happenings
+            happenings.map { happening ->
+                val coordinates = LatLng(happening.location.geometry.coordinates[0].toDouble(), happening.location.geometry.coordinates[1].toDouble())
+                googleMap.addMarker(MarkerOptions().position(coordinates).title(happening.title))
+                println(happening.title)
+            }
+        })
+
+        val cameraPosition = CameraPosition.Builder()
+                .target(stockholm)
+                .zoom(12f)
+                .build()
+
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+
     }
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        val viewModel= ViewModelProviders.of(requireActivity()).get(HappeningsViewModel::class.java)
+
         return inflater.inflate(R.layout.fragment_maps, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //viewModel= ViewModelProviders.of(requireActivity()).get(HappeningsViewModel::class.java)
-
-
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+
     }
 }
